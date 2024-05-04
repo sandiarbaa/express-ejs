ad7f2796b002a947ac72b371c3ef327314636dd1
sandiarbaa <sandiarbaa@gmail.com>
2024-05-04 16:55:34 +0700

    add: initials files

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(8080, () => console.log("Listening on port http://localhost:8080"));
