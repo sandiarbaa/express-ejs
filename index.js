b9324dfa81739062d52ac50670ca02755493abbd
sandiarbaa <sandiarbaa@gmail.com>
2024-05-04 19:51:11 +0700

    add: add & setup static folder for web assets

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const tagsData = require("./data.json");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views")); // main file + views folder
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.render("home");
